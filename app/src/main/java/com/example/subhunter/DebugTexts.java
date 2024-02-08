6edd08fc23d574d660be8aa1179a239859ebb975
Gladiozya <chetl1227@gmail.com>
2024-02-07 21:17:45 -0800

    Debugs into different class

---
+++ app/src/main/java/com/example/subhunter/DebugTexts.java
@@ -0,0 +1,36 @@
+package com.example.subhunter;
+
+public class DebugTexts extends SubHunter{
+
+    private final int size=50;
+    // This code prints the debugging text
+    public void printDebuggingText() {
+        paint.setTextSize(blockSize);
+
+        debugFormula("numberHorizontalPixels", numberHorizontalPixels,3);
+        debugFormula("numberVerticalPixels", numberVerticalPixels, 4);
+
+        debugFormula("blockSize", blockSize, 5);
+
+        debugFormula("gridWidth", gridWidth,6);
+        debugFormula("gridHeight", gridHeight, 7);
+
+        debugFormula("horizontalTouched", horizontalTouched, 8);
+        debugFormula("verticalTouched", verticalTouched,  9);
+
+        debugFormula("subHorizontalPosition", subHorizontalPosition,  10);
+        debugFormula("subVerticalPosition", subVerticalPosition,  11);
+
+        debugFormula("shotsTaken", shotsTaken, 13);
+
+        debugBoolean("hit", hit, 12);
+        debugBoolean("debugging", debugging,  14);
+    }
+    public void debugFormula(String variableName, float variableValue,float blockMultiply){
+        canvas.drawText(variableName +" = " + variableValue, size, blockSize * blockMultiply, paint);
+    }
+
+    public void debugBoolean(String variableName, boolean booleanValue,float blockMultiply){
+        canvas.drawText(variableName +" = " + booleanValue, size, blockSize * blockMultiply, paint);
+    }
+}
